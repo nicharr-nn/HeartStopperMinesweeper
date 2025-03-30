fb7dc3adba98830461d82a2c35a63e5328cd88ff
nicharr-nn <nicha.rua@gmail.com>
2025-03-30 16:31:05 +0700

    added display for board

--- game_ui.py
+++ game_ui.py
@@ -1,15 +1,17 @@
 import pygame as pg
+from player import Player
 
-GRID_SIZE = 10
-TILE_SIZE = 30
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (50, 50, 50)
 TEAL = (115, 199, 199)
 YELLOW = (244, 248, 211)
 PINK = (247, 207, 216)
+LIGHT_PINK = (253, 223, 229)
 LIGHT_TEAL = (166, 241, 224)
 DARK_TEAL = (74,133,135)
+GRID_SIZE = 10
+TILE_SIZE = 40
 
 class GameGUI:
     def __init__(self):
@@ -18,17 +20,24 @@ class GameGUI:
         pg.display.set_caption("Heart Stopper Minesweeper")
         self.__running = True
         self.__game_started = False
+        # self.player = Player()
+
         self.font = pg.font.Font("font/PixeloidMono.ttf", 25)
         self.title_font = pg.font.Font("font/PixeloidMono.ttf", 50)
+
+        self.classic_bomb_img = pg.transform.scale(pg.image.load("image/classic.png"), (TILE_SIZE, TILE_SIZE))
+        self.heartdrain_bomb_img = pg.transform.scale(pg.image.load("image/heartdrain.png"), (TILE_SIZE, TILE_SIZE))
+        self.countdown_bomb_img = pg.transform.scale(pg.image.load("image/countdown.png"), (TILE_SIZE, TILE_SIZE))
+
         self.home_screen()
 
     def home_screen(self):
         """Displays the home screen."""
         self.__screen.fill(YELLOW)
+
         background_text_rect = pg.Rect(100, 100, 600, 400)
         pg.draw.rect(self.__screen, WHITE, background_text_rect, border_radius=50)
 
-        # Render text using the preloaded font
         text_welcome = self.title_font.render("Welcome!", True, DARK_TEAL)
         text_start_btn = self.font.render("Start", True, YELLOW)
         text_quit_btn = self.font.render("Quit", True, YELLOW)
@@ -50,9 +59,33 @@ class GameGUI:
 
         pg.display.update()
 
+
     def display_board(self):
-        """Renders the grid."""
-        pass
+        """Renders the grid. Game screen"""
+        self.__screen.fill(YELLOW)
+
+        heart_img = pg.image.load("image/heart.png")
+        heart_img = pg.transform.scale(heart_img, (30, 30))
+        self.__screen.blit(heart_img, (20, 20))
+
+        font = pg.font.Font("font/PixeloidMono.ttf", 24)
+        hearts_text = font.render(f"x {self.player.hearts}", True, BLACK)
+        self.__screen.blit(hearts_text, (60, 20))
+
+        score_text = font.render(f"COUNTDOWN:", True, BLACK)
+        self.__screen.blit(score_text, (570, 20))
+
+        grid_start_x = (750 - GRID_SIZE * TILE_SIZE) // 2
+        grid_start_y = (600 - GRID_SIZE * TILE_SIZE) // 2
+
+        # Draw grid
+        for x in range(GRID_SIZE):
+            for y in range(GRID_SIZE):
+                tile_rect = pg.Rect(grid_start_x + x * (TILE_SIZE+5), grid_start_y + y * (TILE_SIZE+5), TILE_SIZE, TILE_SIZE)
+                pg.draw.rect(self.__screen, LIGHT_PINK, tile_rect, border_radius=5)
+
+        pg.display.update()
+
 
     def handle_click(self, pos):
         """Handles button clicks on the screen."""
