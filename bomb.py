2744dcb2caaafc222e57fbd481140b00e7f5759e
nicharr-nn <nicha.rua@gmail.com>
2025-05-11 18:45:38 +0700

    removed init from classes

--- bomb.py
+++ bomb.py
@@ -6,22 +6,13 @@ class Bomb:
         pass
 
 class ClassicBomb(Bomb):
-    def __init__(self, bomb_img):
-        super().__init__(bomb_img)
-
     def trigger_effect(self, player):
         player.hearts = 0
 
 class HeartDrainBomb(Bomb):
-    def __init__(self, bomb_img):
-        super().__init__(bomb_img)
-
     def trigger_effect(self, player):
         player.lose_heart()
 
 class CountdownBomb(Bomb):
-    def __init__(self, bomb_img):
-        super().__init__(bomb_img)
-
     def trigger_effect(self, player):
         player.start_countdown()
